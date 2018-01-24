17762d97ddae591bac11eeefe93eff5b191ccb1d
EvanDude47 <evandude47@gmail.com>
2018-01-23 23:19:57 -0500

    inital commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var app = express();
+
+var port = 3000;
+
+app.use(express.static("src"));
+
+//Listen on the port
+app.listen(port, function(err){
+    console.log("Server is running on port " + port);
+});
